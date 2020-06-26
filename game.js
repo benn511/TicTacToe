3969ddb57a0c4c4bebec5cacf4ea160fbb8aa3af
benn511 <bgomez190@gmail.com>
2020-06-26 16:52:34 -0500

    Added  checkWinCondition functions. Setting up loop for gameplay

--- game.js
+++ game.js
@@ -1,45 +1,82 @@
 class ttt {
   constructor() {
-    this.display = [
+    this.magicSquare = [
       [4, 9, 2],
       [3, 5, 7],
       [8, 1, 6],
     ];
-    this.p1Turn = true;
-    this.p1Var = "X";
+    this.boardX = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+    this.boardY = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+    this.player1 = "X";
+    this.player2 = "O";
+    this.currentPlayer = this.player1;
+    this.numTurns = 1;
   }
 
-  displayBoard() {
-    console.log(this.display);
+  resetGame() {
+    this.boardX = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+    this.boardY = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+    this.currentPlayer = this.player1;
+    this.numTurns = 1;
   }
 
-  checkRows() {
+  isRowWin(playerBoard) {
     for (let i = 0; i < 3; i++) {
+      let sum = 0;
       for (let j = 0; j < 3; j++) {
-        // console.log(i, " ", j);
-        // console.log(board[i][j]);
-        if (j + (1 % 3) == 0) {
-        }
+        sum += playerBoard[i][j];
       }
+      if (sum == 15) return true;
     }
+    return false;
   }
 
-  checkCol() {
+  isColWin(playerBoard) {
     for (let i = 0; i < 3; i++) {
+      let sum = 0;
       for (let j = 0; j < 3; j++) {
-        // console.log(i, " ", j);
-        console.log(board[j][i]);
+        sum += playerBoard[j][i];
       }
+      if (sum == 15) return true;
+    }
+    return false;
+  }
+
+  isDiagWin(playerBoard) {
+    let sum = 0;
+    if (playerBoard[0][0] + playerBoard[1][1] + playerBoard[2][2] == 15) {
+      return true;
+    } else if (
+      playerBoard[2][0] + playerBoard[1][1] + playerBoard[0][2] ==
+      15
+    ) {
+      return true;
+    } else {
+      return false;
     }
   }
 
   togglePlayer() {
-    if (this.p1Turn) {
-      this.p1Turn = false;
-      this.p1Var = "O";
+    if (this.currentPlayer === this.player1) {
+      this.currentPlayer = this.player2;
     } else {
-      this.p1Turn = true;
-      this.p1Var = "X";
+      this.currentPlayer = this.player1;
     }
   }
 
@@ -48,8 +85,12 @@ class ttt {
     if (this.checkRows || this.checkCol || this.checkDiag) win = true;
     return win;
   }
-
-  checkDiag() {}
 }
 
 let game = new ttt();
+
+//Game ends when board is full or someone wins
+
+// console.log(`Test board wins on col: ${game.isColWin(game.boardX)}`);
+// console.log(`Test board wins on row: ${game.isRowWin(game.boardX)}`);
+// console.log(`Test board wins on diag: ${game.isDiagWin(game.boardX)}`);
