6c61823237576358fe7dd5bf33791eeab1b6287b
benn511 <bgomez190@gmail.com>
2020-08-30 20:06:40 -0500

    App event delagates and used one handler

--- app.js
+++ app.js
@@ -12,63 +12,70 @@ const statusDiv = document.querySelector(".status");
 const resetDiv = document.querySelector(".reset");
 const cellDivs = document.querySelectorAll(".game-cell");
 const winDiv = document.querySelector(".winner");
+const container = document.querySelector(".container");
 
 //event handlers
-const handleReset = (e) => {
-  game.resetGame();
-  game.checkStatus();
-  console.log(e);
-  //Reset frontend
-  for (const cellDiv of cellDivs) {
-    cellDiv.classList.remove("x");
-    cellDiv.classList.remove("o");
-  }
-  statusDiv.innerHTML = "X is next";
+// const handleReset = (e) => {
+//   game.resetGame();
+//   game.checkStatus();
+//   console.log(e);
+//   //Reset frontend
+//   for (const cellDiv of cellDivs) {
+//     cellDiv.classList.remove("x");
+//     cellDiv.classList.remove("o");
+//   }
+//   statusDiv.innerHTML = "X is next";
+// };
+// const handleCellClick = (e) => {
+//   if (game.gameIsLive && game.numTurns < 9) {
+//     const id = Number(e.target.id);
+//     game.updateGame(id);
+//     game.checkStatus();
+
+//     // updateBoards(e.target.id);
+//     //frontend update
+//     const classList = e.target.classList;
+//     if (classList[1] == "x" || classList[1] == "o") {
+//       return;
+//     }
+//     if (game.xIsNext) {
+//       classList.add("x");
+//       statusDiv.innerHTML = "O is next";
+//     } else {
+//       classList.add("o");
+//       statusDiv.innerHTML = "X is next";
+//     }
+//     //On game over display banner
+//     if (!game.gameIsLive) {
+//       //Display win message
+//       if (game.checkWin(game.boardX)) {
+//         statusDiv.innerHTML = "X has won!";
+//         console.log("X has won!");
+//       } else if (game.checkWin(game.boardY)) {
+//         statusDiv.innerHTML = "O has won!";
+//         console.log("O has won");
+//       } else {
+//         statusDiv.innerHTML = "Game tie!";
+//         console.log("Game has ended in a tie!");
+//       }
+//     }
+//     game.togglePlayer();
+//   } else {
+//     if (game.checkWin(game.boardX) || game.checkWin(game.boardY)) {
+//       console.log("Game over: has been decided already!");
+//     }
+//   }
+// };
+
+const handleClick = (e) => {
+  console.log(e.target);
 };
-const handleCellClick = (e) => {
-  if (game.gameIsLive && game.numTurns < 9) {
-    const id = Number(e.target.id);
-    game.updateGame(id);
-    game.checkStatus();
 
-    // updateBoards(e.target.id);
-    //frontend update
-    const classList = e.target.classList;
-    if (classList[1] == "x" || classList[1] == "o") {
-      return;
-    }
-    if (game.xIsNext) {
-      classList.add("x");
-      statusDiv.innerHTML = "O is next";
-    } else {
-      classList.add("o");
-      statusDiv.innerHTML = "X is next";
-    }
-    //On game over display banner
-    if (!game.gameIsLive) {
-      //Display win message
-      if (game.checkWin(game.boardX)) {
-        statusDiv.innerHTML = "X has won!";
-        console.log("X has won!");
-      } else if (game.checkWin(game.boardY)) {
-        statusDiv.innerHTML = "O has won!";
-        console.log("O has won");
-      } else {
-        statusDiv.innerHTML = "Game tie!";
-        console.log("Game has ended in a tie!");
-      }
-    }
-    game.togglePlayer();
-  } else {
-    if (game.checkWin(game.boardX) || game.checkWin(game.boardY)) {
-      console.log("Game over: has been decided already!");
-    }
-  }
-};
+container.addEventListener("click", handleClick);
 
-//event listeners
-resetDiv.addEventListener("click", handleReset);
+// //event listeners
+// resetDiv.addEventListener("click", handleReset);
 
-for (const cellDiv of cellDivs) {
-  cellDiv.addEventListener("click", handleCellClick);
-}
+// for (const cellDiv of cellDivs) {
+//   cellDiv.addEventListener("click", handleCellClick);
+// }
